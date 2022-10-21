1826347e2f0fb489e36c99dec73977e1ab318952
vidhya kannan <vidhyakannann87@gmail.com>
2022-10-21 14:46:51 -0400

    First commit

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -4,8 +4,14 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.configuration.BotConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * The entry point of the Spring Boot application.
@@ -21,6 +27,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
 public class Application implements AppShellConfigurator {
 
+    @Bean
+    public Bot alice() {
+        return new Bot(BotConfiguration.builder()
+                        .name("alice")
+                        .path("src/main/resources")
+                        .build()
+        );
+    }
+
+
+    @Bean
+    public ScheduledExecutorService executorService() {
+        return Executors.newScheduledThreadPool(2);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/example/application/views/chat/ChatView.java
+++ src/main/java/com/example/application/views/chat/ChatView.java
@@ -1,33 +1,75 @@
 package com.example.application.views.chat;
 
 import com.example.application.views.MainLayout;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.*;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.springframework.context.ApplicationContext;
+import org.vaadin.artur.Avataaar;
+
+import java.util.Random;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @PageTitle("Chat")
 @Route(value = "chat", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
+@CssImport("styles/views/chat/chat-view.css")
 public class ChatView extends VerticalLayout {
 
-    public ChatView() {
-        setSpacing(false);
+    private final UI ui;
+    private final MessageList messageList = new MessageList();
+    private final TextField message = new TextField();
+    private Chat chatSession;
+    private final ScheduledExecutorService executorService;
+    private final ApplicationContext applicationContext;
+    private Bot bot;
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
+    public ChatView(Bot alice, ScheduledExecutorService executorService, ApplicationContext applicationContext) {
+        this.executorService = executorService;
+        this.applicationContext = applicationContext;
+        ui = UI.getCurrent();
+        chatSession = new Chat(alice);
+        sendOpeningMessage();
+        message.setPlaceholder("Enter a message...");
+        message.setSizeFull();
+        Button send = new Button(VaadinIcon.ENTER.create(), event -> sendMessage());
+        send.addClickShortcut(Key.ENTER);
+        HorizontalLayout inputLayout = new HorizontalLayout(message, send);
+        inputLayout.addClassName("inputLayout");
+        add(messageList, inputLayout);
+        expand(messageList);
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
     }
+
+    private void sendMessage() {
+        String text = message.getValue();
+        messageList.addMessage("You", new Avataaar("Name"), text, true);
+        message.clear();
+        String answer = chatSession.multisentenceRespond(text);
+        ui.access(() -> messageList.addMessage(
+                "Alice", new Avataaar("Alice2"), answer, false));
+//        executorService.schedule(() -> {
+//                    String answer = chatSession.multisentenceRespond(text);
+//                    ui.access(() -> messageList.addMessage(
+//                            "Alice", new Avataaar("Alice2"), answer, false));
+//                },new Random().ints(100, 300).findFirst().getAsInt(),
+//              TimeUnit.MILLISECONDS);
+    }
+
+    private void sendOpeningMessage() {
+        String openingMessage = "Welcome to another beautiful day,How are you feeling?";
+        ui.access(() -> messageList.addMessage(
+                "Alice", new Avataaar("Alice2"),openingMessage, false));
+    }
+
 
 }
